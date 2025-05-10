5cb9285fe1fba5fee37adfc6cdee384b3cbde5bc
AmaanNaseh <naseh.amaan@gmail.com>
2025-05-10 15:04:49 +0530

    updated app.py run

--- backend/app.py
+++ backend/app.py
@@ -258,5 +258,9 @@ def check_threat():
 
 
 # -------------------------- Running Application --------------------------
+# if __name__ == "__main__":
+#     app.run(debug=True)
+
 if __name__ == "__main__":
-    app.run(debug=False)
+    port = int(os.environ.get("PORT", 5001))  # Get port from environment, default to 5000
+    app.run(host="0.0.0.0", port=port, debug=False)
